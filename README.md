5318b056dbc9da6bc8804e4d8cba1d0a519131ce
Scott Martin <smartin015@gmail.com>
2018-03-04 14:16:52 -0500

    Mid-state commit; ros control working with single servo motor, old python controller code removed/archived. Some rejiggering of folder names

--- README.md
+++ README.md
@@ -9,8 +9,6 @@
 
 ## Running the Robot
 
-TODO: how to use XMing? How to start / run the code?
-
 `roslaunch ktbot stdr_server_ktbot.launch`
 
 ## ROS Details / References
@@ -23,24 +21,37 @@ STDR simulator launcher
 
 ## Next steps
 
+- Workflow fixes
+  - Make single simulatable kt_core launcher and kt_brain launcher.
+  - Set vim editor spaces
+  - Back up KTbot SD card file
+- Mechanical fixes
+  - Edit bot launch file to startup wheel & lidar nodes
+  - Fix serial permissions & do auto port recognition
+     - `sudo usermod -a -G dialout $USER`
+  - Lidar node should turn lidar spin on & off.
+  - More stable wheel base 
+- Driver fixes
+  - Fix servo settings for drifting servo
+  - PWM control of wheel speed in servo
 - Localization, motion planning, and mapping
   - Research `navigation` and `gmapping` libraries, construct settings files for both of 'em.
   - Using teleop, construct a map
   - Point-and-click SLAM navigation
 - Gazebo
-  - Complete "model structure & requirements", "make a mobile robot", and "add a sensor to a robot" in [Build a robot](http://gazebosim.org/tutorials?cat=build_robot) tutorial set.
-  - Create a "Neato" lidar module and publish it to osrf/gazebo_models
-  - Adapt a model plugin that retrieves the gazebo simulated lidar data and publishes it on a ros topic. (see [gazebo_ros_pkgs tutorials](http://gazebosim.org/tutorials?cat=connect_ros))
   - Create a servo simulator model plugin (LX-16A) that parses/interprets serial data and affects a rotating joint. Consider upstreaming it in gazebo_ros_pkgs.
-  - Create a KTBot-like robot with rotatable wheels and top lidar
   - Construct a .world file to launch the ktbot and register the lidar/servo plugins
-  - Try a full integration test, running the simulated bot using [teleop via ROS](http://wiki.ros.org/stdr_simulator/Tutorials/Using%20turtlebot%20for%20teleoperation)
   - Construct hackerhouse model using [this tutorial](http://gazebosim.org/tutorials?cat=build_world&tut=building_editor) and drive the KTbot around in it.
 - Autonomy
   - Build charge station (switch-based wireless charger)
   - Setup low battery sensing (via servos?)
   - Return-to-base behavior when low on battery or objective complete
   - Think up fun objectives!
+
+For V2
+- Fix bot size & cable routing/management
+- Gear wheels for higher speed
+- ARBS
 
 ## Packages
 
---
+++ catkin_ws/src/ktbot_control/src/ktbot_hw_interface.cpp
@@ -0,0 +1,178 @@
+/*********************************************************************
+ * Software License Agreement (BSD License)
+ *
+ *  Copyright (c) 2015, University of Colorado, Boulder
+ *  All rights reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without
+ *  modification, are permitted provided that the following conditions
+ *  are met:
+ *
+ *   * Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *   * Redistributions in binary form must reproduce the above
+ *     copyright notice, this list of conditions and the following
+ *     disclaimer in the documentation and/or other materials provided
+ *     with the distribution.
+ *   * Neither the name of the Univ of CO, Boulder nor the names of its
+ *     contributors may be used to endorse or promote products derived
+ *     from this software without specific prior written permission.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ *  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ *  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ *  FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ *  COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ *  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ *  BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ *  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ *  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ *  LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ *  ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ *  POSSIBILITY OF SUCH DAMAGE.
+ *********************************************************************/
+
+/* Author: Dave Coleman
+   Desc:   Example ros_control hardware interface blank template for the KTBot
+           For a more detailed simulation example, see sim_hw_interface.cpp
+*/
+
+#include <stdio.h>
+#include <string>
+#include <unistd.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <termios.h>
+#include <stdlib.h> 
+
+#include <ktbot_control/ktbot_hw_interface.h>
+
+using std::string;
+
+#define LEFT_JOINT_ID 0
+// TODO: 1
+#define LEFT_JOINT_SERVO_ID 3
+
+#define RIGHT_JOINT_ID 1
+#define RIGHT_JOINT_SERVO_ID 2
+
+#define RUNNING_AVG_NEW_WEIGHT 0.05
+#define ROLLOVER_THRESHOLD_DEG 100
+#define PI 3.14159265359
+
+// @ 7.4v
+#define MAX_RPM 3.27249235
+
+namespace ktbot_control
+{
+
+KTBotHWInterface::KTBotHWInterface(ros::NodeHandle &nh, urdf::Model *urdf_model)
+  : ros_control_boilerplate::GenericHWInterface(nh, urdf_model)
+{
+  // Discover the serial device 
+  //const char* path = get_path_for_serial_id("1a86_USB2.0-Serial");
+  const char* port = "/dev/ttyUSB0";
+
+  int ser; // file description for the serial port
+  ser = open(port, O_RDWR | O_NOCTTY | O_NDELAY);
+  if(ser == -1) {
+    ROS_FATAL_NAMED("ktbot_hw_interface", "open_port: Unable to open port %s\n", port);
+  } else {
+    fcntl(ser, F_SETFL, 0);
+    ROS_INFO_NAMED("ktbot_hw_interface", "port opened: %s\n", port);
+  }
+  
+  left.attach(ser, LEFT_JOINT_SERVO_ID);
+  right.attach(ser, RIGHT_JOINT_SERVO_ID);
+
+  left_rotations = 0;
+  right_rotations = 0;
+
+  left_prev_deg = 0;
+  right_prev_deg = 0;
+
+  left_prev_vel_cmd = 0;
+  right_prev_vel_cmd = 0;
+
+  ROS_INFO_NAMED("ktbot_hw_interface", "KTBotHWInterface Ready.");
+}
+
+void KTBotHWInterface::read(ros::Duration &elapsed_time)
+{
+
+  float deg;
+  if (left.getPos(&deg)) {
+    double d = deg - left_prev_deg;
+    if (abs(d) > ROLLOVER_THRESHOLD_DEG) {
+      left_rotations += (d > 0) ? -1 : 1;
+    }
+    double newpos = ((360.0*left_rotations) + deg) * PI / 180.0;
+    double newvel = double(newpos - joint_position_[LEFT_JOINT_ID]) / elapsed_time.toSec();
+    joint_velocity_[LEFT_JOINT_ID] = (joint_velocity_[LEFT_JOINT_ID] * (1.0 - RUNNING_AVG_NEW_WEIGHT) + newvel * RUNNING_AVG_NEW_WEIGHT);
+    joint_position_[LEFT_JOINT_ID] = newpos;
+    
+    //ROS_INFO_NAMED("ktbot_hw_interface", "%02f\t%02f\t%02f\t%02f\n", deg, d, left_rotations, newpos);
+    
+    // Note: Joint effort not necessary for velocity
+    left_prev_deg = deg;
+  }
+
+  /*
+  if (right.getPos(&deg)) {
+    joint_position_[RIGHT_JOINT_ID] = deg;
+  }
+  */
+}
+
+void KTBotHWInterface::write(ros::Duration &elapsed_time)
+{
+  // Safety
+  enforceLimits(elapsed_time);
+
+  // Just go by position for now
+  // TODO: Rate-limit writes if no change in position command
+  // ROS_INFO_NAMED("ktbot_hw_interface", "%02f\n", joint_velocity_command_[LEFT_JOINT_ID]);
+  if (left_prev_vel_cmd != joint_velocity_command_[LEFT_JOINT_ID]) {
+    left.setWheelMode(int16_t(joint_velocity_command_[LEFT_JOINT_ID] * 1000 / MAX_RPM));
+    left_prev_vel_cmd = joint_velocity_command_[LEFT_JOINT_ID];
+  }
+  //right.move(joint_position_command_[RIGHT_JOINT_ID], 100);
+
+  // TODO joint_velocity_command_
+}
+
+void KTBotHWInterface::enforceLimits(ros::Duration &period)
+{
+  // ----------------------------------------------------
+  // ----------------------------------------------------
+  // ----------------------------------------------------
+  //
+  // CHOOSE THE TYPE OF JOINT LIMITS INTERFACE YOU WANT TO USE
+  // YOU SHOULD ONLY NEED TO USE ONE SATURATION INTERFACE,
+  // DEPENDING ON YOUR CONTROL METHOD
+  //
+  // EXAMPLES:
+  //
+  // Saturation Limits ---------------------------
+  //
+  // Enforces position and velocity
+  pos_jnt_sat_interface_.enforceLimits(period);
+  //
+  // Enforces velocity and acceleration limits
+  // vel_jnt_sat_interface_.enforceLimits(period);
+  //
+  // Enforces position, velocity, and effort
+  // eff_jnt_sat_interface_.enforceLimits(period);
+
+  // Soft limits ---------------------------------
+  //
+  // pos_jnt_soft_limits_.enforceLimits(period);
+  // vel_jnt_soft_limits_.enforceLimits(period);
+  // eff_jnt_soft_limits_.enforceLimits(period);
+  //
+  // ----------------------------------------------------
+  // ----------------------------------------------------
+  // ----------------------------------------------------
+}
+
+}  // namespace
