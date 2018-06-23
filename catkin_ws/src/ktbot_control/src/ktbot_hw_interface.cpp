80c08c2ae45627da2b78913f45cbc02267cbcfe3
Scottles <smartin015@gmail.com>
2018-06-23 00:33:48 +0000

    fixed the odomz maaaaan

--- catkin_ws/src/ktbot_control/src/ktbot_hw_interface.cpp
+++ catkin_ws/src/ktbot_control/src/ktbot_hw_interface.cpp
@@ -55,7 +55,7 @@ using std::string;
 #define RIGHT_JOINT_ID 1
 #define RIGHT_JOINT_SERVO_ID 1
 
-#define RUNNING_AVG_NEW_WEIGHT 0.05
+#define RUNNING_AVG_NEW_WEIGHT 0.5
 #define ROLLOVER_THRESHOLD_DEG 100
 #define PI 3.14159265359
 
@@ -63,58 +63,56 @@ using std::string;
 #define MAX_RPM 3.27249235
 
 int set_interface_attribs (int fd, int speed, int parity) {
-        struct termios tty;
-        memset (&tty, 0, sizeof tty);
-        if (tcgetattr (fd, &tty) != 0)
-        {
-                printf("error %d from tcgetattr", errno);
-                return -1;
-        }
+  struct termios tty;
+  memset (&tty, 0, sizeof tty);
+  if (tcgetattr (fd, &tty) != 0) {
+    printf("error %d from tcgetattr", errno);
+    return -1;
+  }
 
-        cfsetospeed (&tty, speed);
-        cfsetispeed (&tty, speed);
+  cfsetospeed (&tty, speed);
+  cfsetispeed (&tty, speed);
 
-        tty.c_cflag = (tty.c_cflag & ~CSIZE) | CS8;     // 8-bit chars
-        // disable IGNBRK for mismatched speed tests; otherwise receive break
-        // as \000 chars
-        tty.c_iflag &= ~IGNBRK;         // disable break processing
-        tty.c_lflag = 0;                // no signaling chars, no echo,
-                                        // no canonical processing
-        tty.c_oflag = 0;                // no remapping, no delays
-        tty.c_cc[VMIN]  = 0;            // read doesn't block
-        tty.c_cc[VTIME] = 5;            // 0.5 seconds read timeout
+  tty.c_cflag = (tty.c_cflag & ~CSIZE) | CS8;     // 8-bit chars
+  // disable IGNBRK for mismatched speed tests; otherwise receive break
+  // as \000 chars
+  tty.c_iflag &= ~IGNBRK;         // disable break processing
+  tty.c_lflag = 0;                // no signaling chars, no echo,
+                                  // no canonical processing
+  tty.c_oflag = 0;                // no remapping, no delays
+  tty.c_cc[VMIN]  = 0;            // read doesn't block
+  tty.c_cc[VTIME] = 5;            // 0.5 seconds read timeout
 
-        tty.c_iflag &= ~(IXON | IXOFF | IXANY); // shut off xon/xoff ctrl
+  tty.c_iflag &= ~(IXON | IXOFF | IXANY); // shut off xon/xoff ctrl
 
-        tty.c_cflag |= (CLOCAL | CREAD);// ignore modem controls,
-                                        // enable reading
-        tty.c_cflag &= ~(PARENB | PARODD);      // shut off parity
-        tty.c_cflag |= parity;
-        tty.c_cflag &= ~CSTOPB;
-        tty.c_cflag &= ~CRTSCTS;
+  tty.c_cflag |= (CLOCAL | CREAD);// ignore modem controls,
+                                  // enable reading
+  tty.c_cflag &= ~(PARENB | PARODD);      // shut off parity
+  tty.c_cflag |= parity;
+  tty.c_cflag &= ~CSTOPB;
+  tty.c_cflag &= ~CRTSCTS;
 
-        if (tcsetattr (fd, TCSANOW, &tty) != 0)
-        {
-                printf("error %d from tcsetattr", errno);
-                return -1;
-        }
-        return 0;
+  if (tcsetattr (fd, TCSANOW, &tty) != 0) {
+    printf("error %d from tcsetattr", errno);
+    return -1;
+  }
+  return 0;
 }
 
 void set_blocking (int fd, int should_block) {
-        struct termios tty;
-        memset (&tty, 0, sizeof tty);
-        if (tcgetattr (fd, &tty) != 0)
-        {
-                printf("error %d from tggetattr", errno);
-                return;
-        }
+  struct termios tty;
+  memset (&tty, 0, sizeof tty);
+  if (tcgetattr (fd, &tty) != 0) {
+    printf("error %d from tggetattr", errno);
+    return;
+  }
 
-        tty.c_cc[VMIN]  = should_block ? 1 : 0;
-        tty.c_cc[VTIME] = 5;            // 0.5 seconds read timeout
+  tty.c_cc[VMIN]  = should_block ? 1 : 0;
+  tty.c_cc[VTIME] = 5;            // 0.5 seconds read timeout
 
-        if (tcsetattr (fd, TCSANOW, &tty) != 0)
-                printf("error %d setting term attributes", errno);
+  if (tcsetattr (fd, TCSANOW, &tty) != 0) {
+    printf("error %d setting term attributes", errno);
+  }
 }
 
 
@@ -171,7 +169,7 @@ void KTBotHWInterface::read(ros::Duration &elapsed_time)
     joint_velocity_[LEFT_JOINT_ID] = (joint_velocity_[LEFT_JOINT_ID] * (1.0 - RUNNING_AVG_NEW_WEIGHT) + newvel * RUNNING_AVG_NEW_WEIGHT);
     joint_position_[LEFT_JOINT_ID] = newpos;
 
-    // ROS_INFO_NAMED("ktbot_hw_interface", "%02f\t%02f\t%02f\t%02f\n", deg, d, left_rotations, newpos);
+    // ROS_INFO_NAMED("ktbot_hw_interface", "%02f\t%02f\t%02f\t%02f\t%02f\n", deg, d, left_rotations, newpos, joint_velocity_[LEFT_JOINT_ID]);
 
     // Note: Joint effort not necessary for velocity
     left_prev_deg = deg;
@@ -179,16 +177,16 @@ void KTBotHWInterface::read(ros::Duration &elapsed_time)
     ROS_INFO_NAMED("ktbot_hw_interface", "No data from left wheel\n");
   }
   if (right.getPos(&deg)) {
-    double d = -deg - right_prev_deg;
+    double d = deg - right_prev_deg;
     if (abs(d) > ROLLOVER_THRESHOLD_DEG) {
       right_rotations += (d > 0) ? -1 : 1;
     }
     double newpos = ((360.0*right_rotations) + deg) * PI / 180.0;
     double newvel = double(newpos - joint_position_[RIGHT_JOINT_ID]) / elapsed_time.toSec();
-    joint_velocity_[RIGHT_JOINT_ID] = (joint_velocity_[RIGHT_JOINT_ID] * (1.0 - RUNNING_AVG_NEW_WEIGHT) + newvel * RUNNING_AVG_NEW_WEIGHT);
-    joint_position_[RIGHT_JOINT_ID] = newpos;
+    joint_velocity_[RIGHT_JOINT_ID] = (joint_velocity_[RIGHT_JOINT_ID] * (1.0 - RUNNING_AVG_NEW_WEIGHT) - newvel * RUNNING_AVG_NEW_WEIGHT);
+    joint_position_[RIGHT_JOINT_ID] = -newpos;
 
-    // ROS_INFO_NAMED("ktbot_hw_interface", "%02f\t%02f\t%02f\t%02f\n", deg, d, left_rotations, newpos);
+    // OS_INFO_NAMED("ktbot_hw_interface", "%02f\t%02f\t%02f\t%02f\t%02f\n", deg, d, right_rotations, newpos, joint_velocity_[RIGHT_JOINT_ID]);
 
     // Note: Joint effort not necessary for velocity
     right_prev_deg = deg;
