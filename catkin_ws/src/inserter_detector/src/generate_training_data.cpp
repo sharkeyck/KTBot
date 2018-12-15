a13191c5afb1cc6661f5516aa728acc356516dd3
Scott Martin <smartin015@gmail.com>
2018-12-15 14:08:19 -0500

    Fix up generate_training_data file to include arg parsing and progress bar

--- catkin_ws/src/inserter_detector/src/generate_training_data.cpp
+++ catkin_ws/src/inserter_detector/src/generate_training_data.cpp
@@ -5,6 +5,8 @@
 #include <inserter_detector/scene_segmentation.h>
 #include <pcl_conversions/pcl_conversions.h>
 
+#include <boost/progress.hpp>
+#include <boost/program_options.hpp>
 #include <boost/foreach.hpp>
 #define foreach BOOST_FOREACH
 #include <iostream>
@@ -12,9 +14,13 @@
 rosbag::Bag source_bag;
 rosbag::Bag dest_bag;
 
+namespace po = boost::program_options;
 using pcl::PCLPointCloud2;
 using pcl::PointCloud;
 using pcl::PointXYZ;
+using std::cout;
+using std::endl;
+using std::string;
 
 using namespace inserter_detector;
 
@@ -33,7 +39,7 @@ boost::shared_ptr<sensor_msgs::PointCloud2> convert_cloud(boost::shared_ptr<sens
     pcl::toPCLPointCloud2(*results[0], *result_pc2);
     result_pc2->header = cloud->header; // Copy header to match frame etc.
 
-    boost::shared_ptr<sensor_msgs::PointCloud2> result;
+    boost::shared_ptr<sensor_msgs::PointCloud2> result(new sensor_msgs::PointCloud2());
     pcl_conversions::moveFromPCL(*result_pc2, *result);
     return result;
   }
@@ -42,17 +48,45 @@ boost::shared_ptr<sensor_msgs::PointCloud2> convert_cloud(boost::shared_ptr<sens
 }
 
 int main (int argc, char** argv) {
-  if (argc != 3) {
-    std::cerr << "Usage: gen_training_data <path_to_bag_file> <output_path>" << std::endl;
-    return 1;
+  string infile, outfile;
+
+  try {
+    po::options_description desc("Allowed options");
+    desc.add_options()
+        ("help", "show help message")
+        ("input", po::value<string>(&infile)->required(), "input .bag file")
+        ("output", po::value<string>(&outfile)->required(), "output .bag file");
+
+    po::variables_map vm;
+    po::store(po::parse_command_line(argc, argv, desc), vm);
+
+    if (vm.count("help")) {
+        cout << desc << "\n";
+        return 1;
+    }
+
+    po::notify(vm);
+
+  } catch(std::exception& e) {
+      std::cerr << "Error: " << e.what() << "\n";
+      return 2;
   }
 
-  std::cout << "Reading bag: " << argv[1] << std::endl;
-  source_bag.open(argv[1], rosbag::bagmode::Read);
-  std::cout << "Writing bag: " << argv[1] << std::endl;
-  dest_bag.open(argv[2], rosbag::bagmode::Write);
-  std::vector<std::string> topics = {"/realsense/depth_registered/points", "/inserter/joint_states"};
+
+  std::cout << "Reading bag: " << infile;
+  source_bag.open(infile, rosbag::bagmode::Read);
+
+  std::vector<std::string> topics = {
+    "/realsense/depth_registered/points",
+    "/inserter/joint_states"
+  };
   rosbag::View view(source_bag, rosbag::TopicQuery(topics));
+  std::cout << "(" << view.size() << " entries)" << std::endl;
+
+  std::cout << "Writing bag: " << outfile << std::endl;
+  dest_bag.open(outfile, rosbag::bagmode::Write);
+
+  boost::progress_display show_progress(view.size());
 
   sensor_msgs::JointState::ConstPtr latest_joint_state;
   foreach(rosbag::MessageInstance const m, view) {
@@ -69,7 +103,7 @@ int main (int argc, char** argv) {
       dest_bag.write(topics[1], m.getTime(), js);
     }
 
-    std::cout << ".";
+    ++show_progress;
   }
 
   std::cout << std::endl << "Closing bags" << std::endl;
